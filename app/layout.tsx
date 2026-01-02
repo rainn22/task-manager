527c04422cc10c6d0b86a3308b449ed34bd72c77
rain <sorany.non@student.cadt.edu.kh>
2026-01-02 23:13:17 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Providers from "./providers";
+
+export const metadata: Metadata = {
+  title: "Task Manager",
+  description: "Connect and manage your tasks efficiently.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
